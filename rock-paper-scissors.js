6583d8dbd12e2129a757345ed35462d8b4411da8
Roxanoel <roxane.noel92@gmail.com>
2022-06-10 10:02:33 +0100

    With a switch statement, turned the random numerical value into a string to return

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,18 @@
 function computerPlay() {
     // Generates a random number (either 0, 1, or 2)
     const value = Math.floor(Math.random() * 3)
-    console.log(value);  // debug
+    
+    // Returns string corresponding with value
+    switch(value) {
+        case 0: return "Rock";
+        case 1: return "Paper";
+        case 2: return "Scissors";
+        default: 
+        console.log("Warning, value was out of bounds");
+        return "Value out of bounds";
+    }
 }
+
+console.log(computerPlay());
+
+//function playRound(playerSelection, computerSelection)
