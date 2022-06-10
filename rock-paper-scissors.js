8324bba47a912c868895a93ed6244a417c4474d5
Roxanoel <roxane.noel92@gmail.com>
2022-06-10 10:35:44 +0100

    Wrote the function to play one round: takes in player input, transforms it to make it conform to the format of the computer's selection, then evaluates both to see who wins. Tested to check it works.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,12 +8,10 @@ function computerPlay() {
         case 1: return "paper";
         case 2: return "scissors";
         default: 
-        console.log("Warning, value was out of bounds");
+        console.log("Warning, value chosen by computer was out of bounds");
         return "Value out of bounds";
     }
 }
-
-console.log(computerPlay());
 
 function playRound(playerSelection, computerSelection) {
     
@@ -24,6 +22,33 @@ function playRound(playerSelection, computerSelection) {
     const winMessage = `You win! ${playerSelection} beats ${computerSelection}.`
     const loseMessage =`You lose! ${computerSelection} beats ${playerSelection}.`
     const drawMessage = `It's a draw! You both picked ${playerSelection}.`
+    const errorMessage = `The results could not be determined, since the value entered was invalid.`
 
-    //
+    //Ensure the value entered by the player was valid: TO DO
+    if (!(playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors")) {
+        return errorMessage;
+    }
+
+    // Compare the two passed-in values to determine results
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "rock") return drawMessage;
+            else if (computerSelection === "paper") return loseMessage;
+            else if (computerSelection === "scissors") return winMessage;
+            break;
+        case "paper":
+            if (computerSelection === "rock") return winMessage;
+            else if (computerSelection === "paper") return drawMessage;
+            else if (computerSelection === "scissors") return loseMessage;
+            break;
+        case "scissors":
+            if (computerSelection === "rock") return loseMessage;
+            else if (computerSelection === "paper") return winMessage;
+            else if (computerSelection === "scissors") return drawMessage;
+            break;
+        default:
+            return errorMessage;
+    }
 }
+
+console.log(playRound(computerPlay(), computerPlay()));
